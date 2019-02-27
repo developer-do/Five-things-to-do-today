a3b47c69d335a285a7c78d3277fefe0c0a881338
developer-do <dev-do@naver.com>
2019-02-28 00:46:22 +0900

    error updategit add -A!

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <?php 
 if(_INDEX_ && isset($_SESSION['userName'])) {
   echo '<script src="/js/index.js"></script>';
-} else if(_INDEX_) {
+}
+if(_INDEX_) {
   echo '<script src="/js/common.js"></script>';
 }
 ?>
